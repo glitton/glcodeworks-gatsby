81f6ca9714d2e430163968455499853b44a6d2e7
Generosa Litton <glcodeworks@gmail.com>
2021-03-23 19:44:26 -0700

    init

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import "../styles/reset.css";
+import "../styles/bootstrap.css";
+// import "../styles/jquery.fs.boxer.css";
+// import "../styles/owl.carousel.css";
+// import "../styles/style.css";
+import { Helmet } from "react-helmet";
+
+export default function Home() {
+  return (
+    <>
+      <Helmet>
+        <title>
+          Generosa Litton: Solutions Engineer - Scuba Diver - Bass Player
+        </title>
+      </Helmet>
+      <header></header>
+      <main>
+        <div>Hello World!</div>
+      </main>
+    </>
+  );
+}
+
+// https://www.gatsbyjs.com/docs/porting-an-html-site-to-gatsby/#porting-indexhtml
